d7231e257a45cc5fba5c2d394770275aef89afec
Rafael Coronetti <rafaelcoronetti@gmail.com>
2023-04-17 21:43:51 -0300

    Criado função para consumir endpoint available.  Instalado dataTable react compnent

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,38 +1,48 @@
 import React, { useState, useEffect } from 'react';
 import instance from './axios/config';
+import DataTable  from 'react-data-table-component';
 
 const Home = () => {
-    const [posts, setPosts] = useState({});
+    const [tickers, settickers] = useState({});
+    const [allTickers, setAllTickers] = useState([]);
 
-    const getPosts = async () => {
+const getAvailable = async () => {
+    try {
+        const response = await instance.get('/api/available'); // endpoint
+        const tickersString = response.data.stocks.join(',');
+        setAllTickers(tickersString);
+       
+        
+        
+      
+    }catch(error) {
+        console.log(error);
+    }
+};
+
+    const getTickers = async () => {
         try {
-            const response = await instance.get('/api/quote/PETR4'); // endpoint
-            const data = response.data.results[0];
-            setPosts(data);
+            const response = await instance.get(`/api/quote/${allTickers}`); // endpoint           
+            const data = response.data.results;
+            
+            settickers(data);
         }catch(error) {
             console.log(error);
         }
     };
 
     useEffect(() => {
-        getPosts();
+        getAvailable();
+        getTickers();        
     }, []);
 
+                                                                       
+console.log(tickers);
     return (
         <div>
-            <h1>Ações</h1>
-            {posts.length ===0 ? <p>Carregando...</p>:
-            <div className='post'> 
-            <p>Simbolo {posts.symbol}</p>
-            <p>Nome {posts.shortName}</p>
-            <p>Cotação {posts.regularMarketPrice}</p>
-            <p>Valor de Mercado {posts.marketCap}</p>
-            <p>Volume de Transações {posts.regularMarketVolume}</p>
-            <p>Moeda {posts.currency}</p>
-            <p>Data {posts.regularMarketTime}</p>
-    
-        </div>
-        }
+            <h1>Ações</h1>       
+          
+        
         </div>
     )
 
