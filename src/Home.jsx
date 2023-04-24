9c3821b8fa5c3303e4e735749e764eb48fd7b205
Rafael Coronetti <rafaelcoronetti@gmail.com>
2023-04-23 21:22:42 -0300

    Criado relação entre tabelas em models. Criado testes de associações. Modificado index para receber corretamente dados da api.

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,51 +1,44 @@
 import React, { useState, useEffect } from 'react';
 import instance from './axios/config';
-import DataTable  from 'react-data-table-component';
+import DataTable from 'react-data-table-component';
 
 const Home = () => {
-    const [tickers, settickers] = useState({});
-    const [allTickers, setAllTickers] = useState([]);
+  const [tickers, setTickers] = useState([]);
+  const [allTickers, setAllTickers] = useState('');
 
-const getAvailable = async () => {
+  const getAvailable = async () => {
     try {
-        const response = await instance.get('/api/available'); // endpoint
-        const tickersString = response.data.stocks.join(',');
-        setAllTickers(tickersString);
-       
-        
-        
-      
-    }catch(error) {
-        console.log(error);
+      const response = await instance.get('/api/available'); // endpoint
+      const tickersString = response.data.stocks.join(',');
+      setAllTickers(tickersString);
+      getTickers(); 
+    } catch (error) {
+      console.log(error);
     }
-};
+  };
 
-    const getTickers = async () => {
-        try {
-            const response = await instance.get(`/api/quote/${allTickers}`); // endpoint           
-            const data = response.data.results;
-            
-            settickers(data);
-        }catch(error) {
-            console.log(error);
-        }
-    };
+  const getTickers = async () => {
+    try {
+      const response = await instance.get(`/api/quote/${allTickers}`); // endpoint
+      const data = response.data.results;     
+      setTickers(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    useEffect(() => {
-        getAvailable();
-        getTickers();        
-    }, []);
+  useEffect(() => {
+    getAvailable();
+  }, []);
 
-                                                                       
-console.log(tickers);
-    return (
-        <div>
-            <h1>Ações</h1>       
-          
-        
-        </div>
-    )
-
+  return (
+    <div>
+      <h1>Ações</h1>
+      {/* usar tickers para renderizar tabela, por exemplo */}
+      <p>{tickers.map(r=> r.symbol)}</p>
+    
+    </div>
+  );
 };
 
 export default Home;
